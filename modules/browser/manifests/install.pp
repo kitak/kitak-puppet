05d1ce97451a6864f84118d7daf43f95b804fc73
Keisuke KITA <kei.kita2501@gmail.com>
2013-08-06 22:31:09 +0900

    ブラウザのマニフェストを追加

--- modules/browser/manifests/install.pp
+++ modules/browser/manifests/install.pp
@@ -1,13 +1,23 @@
 class browser::install {
-  package { 'xvfb':
-    ensure => installed,
+  exec { 'install Xvfb':
+    path => ['/usr/bin', '/bin'],
+    command => "apt-get -y install xvfb",
+    unless => "which Xvfb",
   }
-  package { 'firefox':
-    ensure => installed,
+  exec { 'install firefox':
+    path => ['/usr/bin', '/bin'],
+    command => "apt-get -y install firefox",
+    unless => "which firefox",
   }
   exec { 'setup for chrome install':
     path =>  ['/usr/bin', '/bin'],
     command => 'wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -; sh -c \'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list\'; apt-get update',
+    unless => "apt-cache search chrome | grep ^google-chrome-stable",
   }
-
+  exec { 'install chrome':
+    path => ['/usr/bin', '/bin'],
+    command => "apt-get -y install google-chrome-stable",
+    unless => "which google-chrome",
+    require => Exec['setup for chrome install'],
+  }
 }
--- roles/ci_jstest/manifests/init.pp
+++ roles/ci_jstest/manifests/init.pp
@@ -3,6 +3,7 @@ class ci_jstest {
   include ::ndenv
   include ci_jstest::users
   include ci_jstest::install
+  include ::browser
 
      Class['ci_jstest::users']
   -> Class["::ndenv::install"]
