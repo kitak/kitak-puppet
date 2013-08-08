fab4e41e3792878becab7c3180b00572e3a8b83c
Keisuke KITA <kei.kita2501@gmail.com>
2013-08-08 11:53:31 +0900

    起動用のスクリプトを用意

--- modules/altria/manifests/install.pp
+++ modules/altria/manifests/install.pp
@@ -1,13 +1,19 @@
 class altria::install {
-  file { "/home/ci/setup_and_start_altria.sh":
+  file { "/home/ci/setup_altria.sh":
     owner => "ci",
     ensure => present,
-    content => template("altria/setup_and_start.erb"),
+    content => template("altria/setup.erb"),
   }
 
-  exec { 'setup and start altria':
+  file { "/home/ci/start_altria.sh":
+    owner => "ci",
+    ensure => present,
+    content => template("altria/start.erb"),
+  }
+
+  exec { 'setup altria':
     path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin', '/usr/local/sbin'],
-    command     => "sh /home/ci/setup_and_start_altria.sh",
+    command     => "sh /home/ci/setup_altria.sh",
     creates     => "/var/www/altria",
     timeout     => 0,
   }
