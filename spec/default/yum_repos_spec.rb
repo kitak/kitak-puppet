72edfe3bf25e91e1a39b450b7e56a2331fa795c9
Keisuke KITA <kei.kita2501@gmail.com>
2013-11-16 21:35:44 +0900

    fix typo

--- spec/default/yum_repos_spec.rb
+++ spec/default/yum_repos_spec.rb
@@ -10,7 +10,7 @@ describe yumrepo('nginx') do
   it { should be_enabled }
 end
 
-describe yumrepo('tresuredata') do
+describe yumrepo('treasuredata') do
   it { should exist }
   it { should be_enabled }
 end
